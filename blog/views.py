5558e95dee960ec3dd861e95c08e2a9fc4e433e0
Anna Veselova <anna.veselova.97@mail.ru>
2023-09-10 19:27:25 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -50,8 +50,8 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug).popular()
-    comments = Comment.objects.filter(post=post).prefetch_related('author')
+    post = get_object_or_404(Post.objects.popular(), slug=slug)
+    comments = post.comments.prefetch_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
